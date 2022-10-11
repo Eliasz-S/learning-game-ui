334fc16017c9a99ddf967d06e6f89361cdb4cab7
Elias <politico@mail.ru>
2022-10-11 14:02:41 +0300

    element plus imported (full import)

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "element-plus/dist/index.css";
+import ElementPlus from "element-plus";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
