1ba224fff5c3683a5e631903e5863bc44a3aeabe
Alex Syomin <gurufx.43@gmail.com>
2022-10-13 22:03:16 +0300

    carousel added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="home">
-    <h1>Home Page</h1>
+    <h1>Home Page Menu</h1>
+  </div>
+  <div class="block text-center">
+    <el-carousel type="card" height="50vh">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-
 export default {
   name: "HomeView",
 };
