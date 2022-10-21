47949fed7d859135bf9cf8afa5284766e8d9fdfb
nad-trssv <alienlamahope@gmail.com>
2022-10-21 21:29:14 +0300

    Added games list + detail modal + search

---
+++ src/components/UI/InputUI.vue
@@ -0,0 +1,27 @@
+<template>
+  <input :value="modelValue" @input="updateInput" class="input" />
+</template>
+
+<script>
+export default {
+  name: "input-ui",
+  props: {
+    modelValue: [String, Number],
+  },
+  methods: {
+    updateInput(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input {
+  margin-bottom: 10px;
+  padding: 3px 10px;
+  border: 2px solid rgb(93, 115, 216);
+  border-radius: 5px;
+  margin: 1rem 3rem !important;
+}
+</style>
